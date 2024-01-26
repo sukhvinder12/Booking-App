e929368d356ba93e3dcdad9359bfedfb98a3da12
sukhvinder12 <sukhvindersingh1029@gmail.com>
2024-01-26 14:41:23 +0530

    added components

---
+++ public/booking-book/src/pages/Main/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,69 @@
+
+import React, { Component } from "react";
+import { Layout, Menu, Icon } from "antd";
+
+import "antd/dist/antd.css";
+import "./MainLayout.css";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+export default class MainLayout extends Component {
+  state = {
+    collapsed: true
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  };
+
+  render() {
+    return (
+      <Layout id="Layout">
+        <Sider
+          breakpoint="lg"
+          trigger={null}
+          collapsible
+          id="Sider"
+          collapsed={this.state.collapsed}
+          onBreakpoint={broken => {
+            this.setState({
+              collapsed: !this.state.collapsed
+            });
+          }}
+        >
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            id="Sider-Menu"
+          >
+            <Menu.Item key="1">
+              <Icon type="form" />
+              <span>Booking</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="dashboard" />
+              <span>Dashboard</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: "#fff" }}>
+            <Icon
+              className="trigger"
+              style={{ fontSize: "24px", textAlign: "center" }}
+              type={this.state.collapsed ? "menu-unfold" : "menu-fold"}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content id="Home-Content-Container">
+            {this.props.children}
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
+}
